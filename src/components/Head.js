99c4ac1afe6b69e41f0d00a445f7aee763a567c8
tduncan14 <nbatre1@yahoo.com>
2022-06-11 22:12:13 -0700

    keep moving forward

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React,{useEffect,useState}from 'react';
 import'./Head.css'
 
 
 
 const Header = () => {
 
+    const [image,setImage] = useState('/assets/image-hero-desktop.png')
+
+
+    const width = 768
+  
+    
+    
+    console.log(window.innerWidth,'width')
+
+
+    useEffect(()=>{
+
+   
+
+        if(parseInt(window.innerWidth) < width){
+             setImage('/assets/image-hero-mobile.png')
+        }
+
+        if(parseInt(window.innerWidth) > width) {
+            
+           setImage( '/assets/image-hero-desktop.png') 
+        }
+
+    },[image])
 
 
 
@@ -20,9 +44,11 @@ const Header = () => {
  <div className="button">
     
     <button> Get Started </button>
+    <div className="img"> 
 
+      <img src={image} className="smallerImage" />
      
- <div className="img"> <img src='/assets/image-hero-desktop.webp' alt=""/>
+ {/* {  Window.innerWidth < width ? ( <img className="smallerImage" src='/assets/image-hero-mobile.png' alt=""/>) : ( <img src='/assets/image-hero-desktop.webp' alt=""/> )} */}
     </div>
 
  </div>
